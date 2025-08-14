31302114fcc6b8fa951949a5a0d04a80dcc53596
Rupa Netha <iamsairupa06@gmail.com>
2025-08-15 01:02:02 +0530

    add struct for note model

---
+++ src/model.rs
@@ -0,0 +1,25 @@
+use serde::{Deserializze, Serialize};
+
+/// Database model for a note
+#[derive(Debug, Deserializze, Serialize, sqlx::FromRow)]
+#[allow(non_snake_case)]
+pub struct NoteModel {
+    pub id: String,
+    pub title: String,
+    pub content: String,
+    pub is_published: i8,
+    pub created_at: Option<chrono::DateTime<chrono::Utc>>,
+    pub updated_at: Option<chrono::DataTime<chrono::Utc>>,
+}
+
+/// JSON response model for a note
+ #[derive(Debug, Deserialize, Serialize)]
+ #[allow(non_snake_case)]
+ pub struct NoteModelResponse {
+    pub id: String,
+    pub title: String,
+    pub content: String,
+    pub is_published: bool,
+    pub created_at: chrono::DateTime<chrono::Utc>,
+    pub updated_at: chrono::DateTime<chrono::Utc>,
+ } 
